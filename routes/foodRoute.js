fe5fa482b8fe36ae32e237c93c257390710835d8
Ansuman Maharana <ansumanmaharana@Ansumans-MacBook-Air.local>
2025-08-16 16:29:35 +0530

    adding backend

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { addFood, listFood, removeFood, updateStock } from "../controllers/foodController.js";
-import upload from "../middlewares/multer.js";
+import upload from "../middleware/multer.js";
 
 const foodRouter = express.Router();
 
